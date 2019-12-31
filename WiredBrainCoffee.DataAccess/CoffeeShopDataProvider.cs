de61d092982892b594831039e29949cb83c5e993
Sam Lawther <Sam.Lawther@biofiredx.com>
2019-12-31 12:04:02 -0700

    Add data access project to solution

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfurt", BeansStockInKg = 107 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansStockInKg = 23 };
+            yield return new CoffeeShop { Location = "Munich", BeansStockInKg = 56 };
+        }
+    }
+}
